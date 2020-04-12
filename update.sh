cc8149536257bff2fb3afd80ab24358c32d19f06
Cal-Hagner <caleb.hagner@pm.me>
2020-04-12 19:49:51 -0400

    Add update script

---
+++ update.sh
@@ -0,0 +1,15 @@
+# Remove old files
+rm -rf index.html
+rm -rf favicon.png
+rm -rf assets
+rm -rf main.dart.js
+rm -rf flutter_service_worker.js
+rm -rf main.dart.js.map
+
+# Copy over new files
+cp -r build/web/assets
+cp -r build/web/favicon.png
+cp -r build/web/flutter_service_worker.js
+cp -r build/web/index.html
+cp -r build/web/main.dart.js
+cp -r build/web/main.dart.js.map
