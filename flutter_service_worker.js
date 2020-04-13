d017a147281a4b5649f42a8f8e01818fbdacd37f
GitHub Pages Publisher <action@loganboisvert.com>
2020-04-13 00:06:31 +0000

    Publish to GitHub Pages

---
+++ flutter_service_worker.js
@@ -0,0 +1,38 @@
+'use strict';
+const CACHE_NAME = 'flutter-app-cache';
+const RESOURCES = {
+  "/favicon.png": "5dcef449791fa27946b3d35ad8803796",
+"/manifest.json": "71764ea01cb0c5f765cf4d88a86ca409",
+"/icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
+"/icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
+"/assets/AssetManifest.json": "99914b932bd37a50b983c5e7c90ae93b",
+"/assets/LICENSE": "f16688f43ded14fd526aae2c52783b7e",
+"/assets/fonts/MaterialIcons-Regular.ttf": "56d3ffdef7a25659eab6a68a3fbfaf16",
+"/assets/FontManifest.json": "580ff1a5d08679ded8fcf5c6848cece7",
+"/main.dart.js": "bcd0d3d24227236991eb771626e24a8b",
+"/index.html": "e62a49d4cb42973a2f78f05738407370"
+};
+
+self.addEventListener('activate', function (event) {
+  event.waitUntil(
+    caches.keys().then(function (cacheName) {
+      return caches.delete(cacheName);
+    }).then(function (_) {
+      return caches.open(CACHE_NAME);
+    }).then(function (cache) {
+      return cache.addAll(Object.keys(RESOURCES));
+    })
+  );
+});
+
+self.addEventListener('fetch', function (event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function (response) {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
+  );
+});
