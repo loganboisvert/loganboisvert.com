2c07b692f8a98d20c7f8e935ebc1bb4320420fea
GitHub Pages Publisher <action@loganboisvert.com>
2020-04-18 23:01:46 +0000

    Publish to GitHub Pages

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -1,21 +1,20 @@
 'use strict';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
-  "index.html": "4dbf97fd549d7cbc418b87c44877aa24",
-"/": "4dbf97fd549d7cbc418b87c44877aa24",
+  "index.html": "b63a5bb01a9d308287bffe73cd82a99d",
+"/": "b63a5bb01a9d308287bffe73cd82a99d",
 "icons/favicon_512x512.png": "c13d4f99f37ece68d8eb9d5595f346e9",
 "icons/favicon_180x180.png": "79cab06680ec6ed4ba48b073ed564cc7",
 "icons/browserconfig.xml": "b19019befc628679ada8d76b82040424",
 "icons/favicon_192x192.png": "7fb6dbaa78864ad5147278e8fd7bd67f",
-"icons/favicon_16x16.png": "f22e2980b0d18abb2dbdeb95c344f838",
 "icons/favicon_270x270.png": "c3093bcdc87271f6d1368526709f662f",
-"icons/favicon_32x32.png": "83b77edd8852962b8d29f7b8fd13e7ab",
 "assets/packages/material_design_icons_flutter/lib/fonts/materialdesignicons-webfont.ttf": "6acc7d6cffa21f6eda4baaa90e835fd2",
 "assets/assets/fonts/googleFonts/Fjalla-One/Fjalla-One-400.ttf": "6d03f83cb1e9751dfe8c4b7bdd0cf632",
 "assets/FontManifest.json": "6d72a84c631f1b111f699d23a5600f5a",
 "assets/fonts/MaterialIcons-Regular.ttf": "56d3ffdef7a25659eab6a68a3fbfaf16",
 "assets/AssetManifest.json": "581ef7a3e3062398af2a3aa885e167c9",
 "assets/LICENSE": "fc6f479da435a309aa95ba588a05307d",
+"favicon.png": "83b77edd8852962b8d29f7b8fd13e7ab",
 "main.dart.js": "7b661370b0080c7dcf689a3b85749546",
 "manifest.json": "be2ce9b674a6c1cc4741108fae01813d"
 };
